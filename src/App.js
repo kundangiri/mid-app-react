6fdd24ddc4d7f5cc8596839d646d1ba595ea8358
palhimalaya <himalayapal123@gmail.com>
2022-07-03 14:50:38 +0545

    added billing list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import BillingList from "./constants/BillingList";
 import CashFlowTracker from "./constants/CashFlowTracker";
 import ExpenseTracker from "./constants/ExpenseTracker";
+import ProductTracker from "./constants/ProductsList";
 import Register from "./constants/StudentsRegister";
+import { useState } from "react";
 
 function App() {
+  const [listedProducts, setListedProducts] = useState([]);
   return (
     <div className="App">
       {/* <Register /> */}
       {/* <ExpenseTracker /> */}
-      <CashFlowTracker />
+      {/* <CashFlowTracker /> */}
+      <ProductTracker
+        products={listedProducts}
+        setProducts={setListedProducts}
+      />
+      <BillingList products={listedProducts} />
     </div>
   );
 }
